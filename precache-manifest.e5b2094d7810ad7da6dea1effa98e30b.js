d0e06fb5ff86246b6cfb2c50aef129dbfef20187
Sugguna <venu.sugguna@capgemini.com>
2020-09-26 16:50:47 +0530

    Updates

---
+++ precache-manifest.e5b2094d7810ad7da6dea1effa98e30b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70371f273e4f840e62570be57e91b3fb",
+    "url": "/tv-shows/index.html"
+  },
+  {
+    "revision": "c735797928b33f6920e7",
+    "url": "/tv-shows/static/css/2.43079d8c.chunk.css"
+  },
+  {
+    "revision": "2c734a141e0fbbdf6bd2",
+    "url": "/tv-shows/static/css/main.ad28e8ae.chunk.css"
+  },
+  {
+    "revision": "c735797928b33f6920e7",
+    "url": "/tv-shows/static/js/2.f11ae549.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/tv-shows/static/js/2.f11ae549.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c734a141e0fbbdf6bd2",
+    "url": "/tv-shows/static/js/main.0518c94e.chunk.js"
+  },
+  {
+    "revision": "28b8c78ea779eb2b3067",
+    "url": "/tv-shows/static/js/runtime-main.49286ea2.js"
+  },
+  {
+    "revision": "d6af9c9e61260c8488a5980224b7cc61",
+    "url": "/tv-shows/static/media/no-img-portrait-text.d6af9c9e.png"
+  }
+]);
